03c31cef4fa62ced3e991623b49147161f3aba71
prit1626 <pritchovatiya1626@gmail.com>
2026-03-14 06:12:13 +0530

    change port

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8081/api/v1/';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1/';
 
 const axiosClient = axios.create({
     baseURL,
